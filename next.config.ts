b4fc712f5e865b34e7f750e2cef6297f1e6fd77a
Your Name <illusionaryworldguide@gmail.com>
2025-09-09 19:58:46 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -35,7 +35,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https//',
+        protocol: 'https',
         hostname: 'cdn.youporn.com',
         port: '',
         pathname: '/**',
